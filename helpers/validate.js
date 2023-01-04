a630ae50f75819620e8b5b34abeaecb51064f2ac
David Tavadze <tavadzed@gmail.com>
2023-01-05 03:15:19 +0400

    prettify

--- helpers/validate.js
+++ helpers/validate.js
@@ -12,7 +12,7 @@ const validator = async (body, rules, customMessages, callback) => {
 
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/;
 
-// Tighten password policy
+// tighten password policy
 Validator.register(
     'strict', value => passwordRegex.test(value),
     'Password must contain at least one uppercase letter, one lowercase letter and one number'
@@ -20,20 +20,23 @@ Validator.register(
 
 
 /**
- * Checks if incoming value already exist for unique and non-unique fields in the database
+ * checks if incoming value already exist for unique and non-unique fields in the database
  * e.g email: required|email|exists:User,email
  */
 Validator.registerAsync('exist', function (value, attribute, req, passes) {
     if (!attribute) throw new Error('Specify Requirements i.e fieldName: exist:table,column');
-    //split table and column
+    
+    // split table and column
     let attArr = attribute.split(",");
     if (attArr.length !== 2) throw new Error(`Invalid format for validation rule on ${attribute}`);
 
-    //assign array index 0 and 1 to table and column respectively
+    // assign array index 0 and 1 to table and column respectively
     const { 0: table, 1: column } = attArr;
-    //define custom error message
+    
+    // define custom error message
     let msg = (column == "username") ? `${column} has already been taken ` : `${column} already in use`
-    //check if incoming value already exists in the database
+    
+    // check if incoming value already exists in the database
     Models[table].valueExists({ [column]: value })
     .then((result) => {
         if (result) {
--- models/txs/transactions.js
+++ models/txs/transactions.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const transactions = new mongoose.Schema(
   {
     from: String,
-    to:String,
-    ammount:Number,
-    tx_type:String
+    to: String,
+    ammount: Number,
+    tx_type: String
   },
   {
     timestamps: true,
