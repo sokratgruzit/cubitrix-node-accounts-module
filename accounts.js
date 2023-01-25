76adf4f7b63894e4d7ad855bfc4319befcaa903b
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-01-25 16:26:23 +0400

    changed amin js and removed models

--- accounts.js
+++ accounts.js
@@ -1,7 +1,5 @@
-const accounts = require("./controllers/accounts_controller");
-const accounts_index = require("./routes/index");
+const accounts = require("./routes/index");
 
 module.exports = {
   accounts: accounts,
-  accounts_index: accounts_index,
 };
